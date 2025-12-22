ec459a36af3fdced20721f7b048e20ca9f163cd4
Abhisek Bakshi <bakshiabhisek@gmail.com>
2025-12-22 19:57:38 +0530

    Refactor footer for web app

--- apps/web/src/pages/narisangha/footer/Footer.jsx
+++ apps/web/src/pages/narisangha/footer/Footer.jsx
@@ -1,13 +1,11 @@
 import React from "react";
 import FooterCopyright from "./FooterCopyright";
-import FooterLinks from "./FooterLinks";
 
 const Footer = ({ setViewMode }) => {
   return (
     <footer className="site-footer">
       <div className="footer-inner">
         <FooterCopyright />
-        <FooterLinks setViewMode={setViewMode} />
       </div>
     </footer>
   );
