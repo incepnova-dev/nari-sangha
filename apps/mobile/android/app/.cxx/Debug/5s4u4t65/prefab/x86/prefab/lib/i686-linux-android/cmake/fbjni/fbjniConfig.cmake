173b3e19dc1c17f40356809efe72f5f5d588fc12
Abhisek Bakshi <bakshiabhisek@gmail.com>
2025-12-23 10:16:03 +0530

    Remove .cxx build artifacts from git tracking

--- apps/mobile/android/app/.cxx/Debug/5s4u4t65/prefab/x86/prefab/lib/i686-linux-android/cmake/fbjni/fbjniConfig.cmake
+++
@@ -1,9 +0,0 @@
-if(NOT TARGET fbjni::fbjni)
-add_library(fbjni::fbjni SHARED IMPORTED)
-set_target_properties(fbjni::fbjni PROPERTIES
-    IMPORTED_LOCATION "/Users/abhisekbakshi/.gradle/caches/8.13/transforms/eebb435d0fa1db616a6d3eb1ab149109/transformed/jetified-fbjni-0.7.0/prefab/modules/fbjni/libs/android.x86/libfbjni.so"
-    INTERFACE_INCLUDE_DIRECTORIES "/Users/abhisekbakshi/.gradle/caches/8.13/transforms/eebb435d0fa1db616a6d3eb1ab149109/transformed/jetified-fbjni-0.7.0/prefab/modules/fbjni/include"
-    INTERFACE_LINK_LIBRARIES ""
-)
-endif()
-
--- apps/mobile/android/app/.cxx/Debug/5s4u4t65/prefab/x86_64/prefab/lib/x86_64-linux-android/cmake/hermes-engine/hermes-engineConfig.cmake
+++
@@ -1,9 +0,0 @@
-if(NOT TARGET hermes-engine::hermesvm)
-add_library(hermes-engine::hermesvm SHARED IMPORTED)
-set_target_properties(hermes-engine::hermesvm PROPERTIES
-    IMPORTED_LOCATION "/Users/abhisekbakshi/.gradle/caches/8.13/transforms/d3fd3e7fa8f531bb8ce67a274b45539a/transformed/jetified-hermes-android-0.82.0-debug/prefab/modules/hermesvm/libs/android.x86_64/libhermesvm.so"
-    INTERFACE_INCLUDE_DIRECTORIES "/Users/abhisekbakshi/.gradle/caches/8.13/transforms/d3fd3e7fa8f531bb8ce67a274b45539a/transformed/jetified-hermes-android-0.82.0-debug/prefab/modules/hermesvm/include"
-    INTERFACE_LINK_LIBRARIES ""
-)
-endif()
-
