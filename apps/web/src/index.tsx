ccdddd337e17e0c8b5286e64eca4fab9d27766f6
Abhisek Bakshi <bakshiabhisek@gmail.com>
2025-12-24 22:57:46 +0530

    refactoring css for web application

--- apps/web/src/index.tsx
+++ apps/web/src/index.tsx
@@ -1,7 +1,7 @@
 // @ts-nocheck
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import './styles/global/index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
